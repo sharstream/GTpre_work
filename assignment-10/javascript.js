82b1a77dc06b561f740d6b9b99a1a1a702e5c2cd
David Perez <david.perez@us.abb.com>
2017-12-17 18:11:37 -0500

    assignment 10 using javascript

---
+++ assignment-10/javascript.js
@@ -0,0 +1,24 @@
+
+function growFunction() {
+    document.getElementById("box").style.height = "250px";
+}
+
+document.getElementById("colorChg").addEventListener("click", function () {
+
+    document.getElementById("box").style.backgroundColor = "blue";
+
+});
+
+document.getElementById("opacityBtn").addEventListener("click", function () {
+
+    document.getElementById("box").style.opacity = 0.5;
+
+});
+
+document.getElementById("resetBtn").addEventListener("click", function () {
+
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = 1;
+
+});
